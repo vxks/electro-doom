d8d5803ba23adf8b54fbc34169ca0440c43db47f
Vitalii Kalinichev <vxksoftware@gmail.com>
2023-02-18 19:44:18 -0800

    Measure currents

--- src/main/scala/Circuit.scala
+++ src/main/scala/Circuit.scala
@@ -4,8 +4,16 @@ sealed trait Circuit {
   self =>
 
   import Circuit.*
+  import CircuitComponent.*
   import Units.*
   import Resistance.*
+
+  def transform[B](f: CircuitComponent => CircuitComponent): Circuit =
+    //TODO: tailrec
+    self match
+      case Series(cs*)                 => Series(cs.map(_.transform(f)): _*)
+      case Parallel(cs*)               => Parallel(cs.map(_.transform(f)): _*)
+      case component: CircuitComponent => f(component)
 
   lazy val resistanceEq: Option[Resistance] =
     self match
@@ -22,26 +30,28 @@ sealed trait Circuit {
         }
         maybeRs.map(rs => (1 / rs.map(_.inverse).sum).Ohms)
 
-      case Resistor(_, value)      => Some(value)
-      case VoltageSource(_, value) => None
+      case cc: CircuitComponent =>
+        cc match
+          case Resistor(_, value)  => Some(value)
+          case VoltageSource(_, _) => None
 
-  /**
-   * Assumes total voltage drop == V_in
-   */
-  def voltagesC(V_in: Voltage): Map[Resistor, Voltage] = {
+  def collectMeasurements(input: Voltage | Current): Map[Resistor, (Voltage, Current)] = {
     import Formulas.*
 
     val R_eq = self.resistanceEq
       .getOrElse(throw new IllegalArgumentException("Short circuit, WYD!?!?"))
-    val I_t = I(V_in, R_eq)
+
+    val (v_in, i_t) = input match
+      case v: Voltage => (v, I(v, R_eq))
+      case c: Current => (V(c, R_eq), c)
 
     //TODO: make tail recursive
     def loop(
       v_t: Voltage,
       i_t: Current,
       cir: Circuit,
-      agg: Map[Resistor, Voltage]
-    ): Map[Resistor, Voltage] =
+      agg: Map[Resistor, (Voltage, Current)]
+    ): Map[Resistor, (Voltage, Current)] =
       cir match {
         // I constant
         case Series(cs*) =>
@@ -61,21 +71,36 @@ sealed trait Circuit {
             loop(v_c, i_c, c, Map())
           }.toMap ++ agg
 
-        case r: Resistor      => agg + (r -> V(i_t, r.value))
-        case _: VoltageSource => agg
+        case circuitComponent: CircuitComponent =>
+          circuitComponent match
+            case r: Resistor      => agg + (r -> (V(i_t, r.value), I(v_t, r.value)))
+            case _: VoltageSource => agg
       }
 
-    loop(V_in, I_t, self, Map())
+    loop(v_in, i_t, self, Map())
   }
+
+  /**
+   * Assumes total voltage drop == V_in
+   */
+  def collectVoltages(V_in: Voltage): Map[Resistor, Voltage] =
+    collectMeasurements(V_in).view.mapValues(_._1).toMap
+
+  def collectCurrents(I_total: Current): Map[Resistor, Current] =
+    collectMeasurements(I_total).view.mapValues(_._2).toMap
 }
 
 object Circuit:
-  case class Series(cs: Circuit*)                          extends Circuit
-  case class Parallel(cs: Circuit*)                        extends Circuit
-  case class VoltageSource(id: Long, value: Units.Voltage) extends Circuit
-  case class Resistor(id: Long, value: Units.Resistance)   extends Circuit
+  case class Series(cs: Circuit*)   extends Circuit
+  case class Parallel(cs: Circuit*) extends Circuit
 
-  val S: Series.type         = Series
-  val P: Parallel.type       = Parallel
-  val VS: VoltageSource.type = VoltageSource
-  val R: Resistor.type       = Resistor
+  sealed trait CircuitComponent extends Circuit
+  object CircuitComponent {
+    case class VoltageSource(id: Long, value: Units.Voltage) extends CircuitComponent
+    case class Resistor(id: Long, value: Units.Resistance)   extends CircuitComponent
+  }
+
+  val S: Series.type                          = Series
+  val P: Parallel.type                        = Parallel
+  val VS: CircuitComponent.VoltageSource.type = CircuitComponent.VoltageSource
+  val R: CircuitComponent.Resistor.type       = CircuitComponent.Resistor
--- src/test/scala/CircuitSpec.scala
+++ src/test/scala/CircuitSpec.scala
@@ -11,7 +11,7 @@ class CircuitSpec extends AnyFunSpec with Matchers {
 
   describe("Circuit") {
 
-    describe("voltage") {
+    describe("voltages") {
       it("must collect series circuit voltage measurements") {
         val expected = Map(
           R(1, 1.0.Ohms) -> 0.125.Volts,
@@ -19,14 +19,14 @@ class CircuitSpec extends AnyFunSpec with Matchers {
           R(3, 4.0.Ohms) -> 0.5.Volts,
           R(4, 1.0.Ohms) -> 0.125.Volts
         )
-        val result = circuitS.voltagesC(1.Volts)
+        val result = circuitS.collectVoltages(1.Volts)
         result mustBe expected
       }
 
       it("must collect parallel circuit voltage measurements") {
         val rs       = resistors.take(5)
         val expected = rs.map(_ -> 1.0.Volts).toMap
-        val result   = P(rs: _*).voltagesC(1.Volts)
+        val result   = P(rs: _*).collectVoltages(1.Volts)
         result mustBe expected
       }
 
@@ -37,7 +37,40 @@ class CircuitSpec extends AnyFunSpec with Matchers {
           R(3, 1.0.Ohms) -> 0.2.Volts,
           R(4, 1.0.Ohms) -> 0.4.Volts
         )
-        val result = circuitMixed.voltagesC(1.Volts)
+        val result = circuitMixed1.collectVoltages(1.Volts)
+        result mustBe expected
+      }
+    }
+
+    describe("currents") {
+      it("must collect parallel circuit current measurements") {
+        val expected = Map(
+          R5 -> 0.3.Amps,
+          R6 -> 0.2.Amps,
+          R7 -> 0.3.Amps,
+          R8 -> 0.2.Amps
+        )
+        val result = circuitP2.collectCurrents(1.Amps)
+        result mustBe expected
+      }
+
+      it("must collect series circuit current measurements") {
+        val rs       = resistors.take(5)
+        val expected = rs.map(_ -> 0.1.Amps).toMap
+        val result   = S(rs: _*).collectCurrents(0.1.Amps)
+        result mustBe expected
+      }
+
+      it("must collect mixed circuit current measurements") {
+        val expected = Map(
+          R1 -> 1.Amps,
+          R2 -> 1.Amps,
+          R5 -> 0.3.Amps,
+          R6 -> 0.2.Amps,
+          R7 -> 0.3.Amps,
+          R8 -> 0.2.Amps
+        )
+        val result = circuitMixed2.collectCurrents(1.Amps)
         result mustBe expected
       }
     }
@@ -45,6 +78,8 @@ class CircuitSpec extends AnyFunSpec with Matchers {
 }
 
 object CircuitSpec:
+  import CircuitComponent.*
+  
   private def genResistance(): Resistance = Random.nextFloat().Ohms
 
   val resistors: LazyList[Resistor] =
@@ -55,20 +90,20 @@ object CircuitSpec:
       )
     }
 
-  val circuitS: Circuit = Series(
-    R(1, 1.Ohms),
-    R(2, 2.Ohms),
-    R(3, 4.Ohms),
-    R(4, 1.Ohms)
-  )
-  val circuitP: Circuit = Parallel(
-    R(1, 1.Ohms),
-    R(2, 2.Ohms),
-    R(3, 4.Ohms),
-    R(4, 1.Ohms)
-  )
+  val R1 = R(1, 1.Ohms)
+  val R2 = R(2, 2.Ohms)
+  val R3 = R(3, 4.Ohms)
+  val R4 = R(4, 1.Ohms)
+  val R5 = R(1, 2.0.Ohms)
+  val R6 = R(2, 3.0.Ohms)
+  val R7 = R(3, 2.0.Ohms)
+  val R8 = R(4, 3.0.Ohms)
 
-  val circuitMixed: Circuit = Series(
+  val circuitS: Circuit  = Series(R1, R2, R3, R4)
+  val circuitP1: Circuit = Parallel(R1, R2, R3, R4)
+  val circuitP2: Circuit = Parallel(R5, R6, R7, R8)
+
+  val circuitMixed1: Circuit = Series(
     R(1, 1.Ohms),
     Parallel(
       R(2, 1.Ohms),
@@ -76,3 +111,10 @@ object CircuitSpec:
     ),
     R(4, 1.Ohms)
   )
+
+  val circuitMixed2: Circuit =
+    S(
+      R1,
+      P(R5, R6, R7, R8),
+      R2
+    )
